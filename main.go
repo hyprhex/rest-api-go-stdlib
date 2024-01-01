61300940e5b928411950bcdf87c7fff6a855b280
HyprHex <swapapis@gmail.com>
2024-01-01 10:04:26 +0100

    Routing REST requests

--- main.go
+++ main.go
@@ -1,6 +1,49 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
+
+type homeHandler struct{}
+type RecipesHandler struct{}
+
+var (
+    RecipeRe       = regexp.MustCompile(`^/recipes/*$`)
+    RecipeReWithID = regexp.MustCompile(`^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
+)
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is my home page"))
+}
+
+func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+    case r.Method == http.MethodPost && RecipeRe.MatchString(r.URL.Path):
+        h.CreateRecipe(w, r)
+        return
+    case r.Method == http.MethodGet && RecipeRe.MatchString(r.URL.Path):
+        h.ListRecipes(w, r)
+        return
+    case r.Method == http.MethodGet && RecipeReWithID.MatchString(r.URL.Path):
+        h.GetRecipe(w, r)
+        return
+    case r.Method == http.MethodPut && RecipeReWithID.MatchString(r.URL.Path):
+        h.UpdateRecipe(w, r)
+        return
+    case r.Method == http.MethodDelete && RecipeReWithID.MatchString(r.URL.Path):
+        h.DeleteRecipe(w, r)
+        return
+    default:
+        return
+    }
+}
+
+func (h *RecipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
 	// Create a new request multiplexer
@@ -8,16 +51,14 @@ func main() {
 
 	// Register the routes and handlers
 	mux.Handle("/", &homeHandler{})
+	mux.Handle("/recipes", &RecipesHandler{})
+	mux.Handle("/recipes/", &RecipesHandler{})
 
 	// Run the server
 	http.ListenAndServe(":8080", mux)
 }
 
-type homeHandler struct{}
 
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my home page"))
-}
 
 
 
